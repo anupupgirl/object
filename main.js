bf22a6fc1eea5ca0385399aa0a2048c2d8221adf
anupupgirl <86975247+anupupgirl@users.noreply.github.com>
2022-03-26 11:03:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+  img = loadImage("dog_cat.jpg");
+}
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+  document.getElementById("status").innerHTML = "Status - Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("modelLoaded");
+  status = true;
+  objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+
+  if (status != "") {
+    for (let i = 0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML =
+        "Status : Objects Dectected";
+      fill("red");
+      percent = floor(objects[i].confidence * 100);
+      textSize(25);
+      text(
+        objects[i].label + " " + percent + " %",
+        objects[i].x + 15,
+        objects[i].y + 25
+      );
+      noFill();
+      stroke("red");
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
+}
